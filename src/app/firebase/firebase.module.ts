e2bf3e73f66af1b614f6834c29020a2832bbe730
Eduardo Miguel Victoria Sánchez <eduardo.mvisa@gmail.com>
2025-05-12 11:41:54 -0600

    Update firebase.module.ts

--- src/app/firebase/firebase.module.ts
+++ src/app/firebase/firebase.module.ts
@@ -7,15 +7,7 @@ import { getAnalytics } from 'firebase/analytics';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyD7hWNdyHq8SEwZN_y6De2ASYrs1rCo6mc',
-  authDomain: 'appdeveloper-b0a81.firebaseapp.com',
-  projectId: 'appdeveloper-b0a81',
-  storageBucket: 'appdeveloper-b0a81.firebasestorage.app',
-  messagingSenderId: '1050971189495',
-  appId: '1:1050971189495:web:3174fee57e6d31d8fc23f2',
-  measurementId: 'G-14PXBPZ4FB',
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
